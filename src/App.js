9adcc05bf7a97404ff11be50d01112c901215248
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-07-10 19:52:36 -0600

    initial upload - functionality with no styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState} from "react";
+import ShowRemainingTime from "./components/ShowRemainingTime";
 
 function App() {
+  const [minimumTime, updateMinimumTime] = useState(12);
+  const [maximumTime, updateMaximumTime] = useState(30);
+
+  const [timeComplete, updateTimeComplete] = useState(false);
+  const [validationError, updateValidationError] = useState("");
+  const [timeEnd, updateTimeEnd] = useState(0);
+  const [showRemaining, updateShowRemaining] = useState(false);
+  const [stopCounting, updateStopCounting] = useState(false);
+
+  const handleBeginTimer = (min, max) => {
+    updateStopCounting(false);
+    if (max < min) {
+      updateValidationError("maximum time must be greater than minimum time");
+      setTimeout(() => {
+        updateValidationError("");
+      }, 4000);
+      return;
+    }
+
+    //do math to get the random time, save to variable
+    const randomTimeMilliSeconds =
+      // Math.floor(Math.random() * (max - min + 1) + min) * 1000; //seconds
+      Math.floor(Math.random() * (max - min + 1) + min) * 1000 * 60; //minutes
+
+    console.log(`randomTimeMilliSeconds: ${randomTimeMilliSeconds}`);
+
+    //save timeEnd
+    const timerEndsAt = Date.now() + randomTimeMilliSeconds;
+    console.log(`timerEndsAt: ${timerEndsAt}`);
+    updateTimeEnd(timerEndsAt);
+
+    //setTimeout
+    setTimeout(() => {
+      updateTimeComplete(true);
+      updateTimeEnd(0);
+      updateStopCounting(true);
+      updateShowRemaining(false);
+    }, randomTimeMilliSeconds);
+  };
+
+  const handleReset = () => {
+    updateStopCounting(true);
+    updateShowRemaining(false);
+    updateTimeComplete(false);
+    updateTimeEnd(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>random timer</h1>
+      <p>will go off between</p>
+
+      <div>
+        <input
+          type="number"
+          min={1}
+          max={55}
+          value={minimumTime}
+          onChange={(e) => updateMinimumTime(Number(e.target.value))}
+        />
+        and
+        <input
+          type="number"
+          min={2}
+          max={60}
+          value={maximumTime}
+          onChange={(e) => updateMaximumTime(Number(e.target.value))}
+        />
+        minutes
+        {timeEnd === 0 && (
+          <button onClick={() => handleBeginTimer(minimumTime, maximumTime)}>
+            Start!
+          </button>
+        )}
+        <button onClick={() => handleReset()}>reset</button>
+        {timeEnd > 0 && (
+          <button onClick={() => updateShowRemaining(!showRemaining)}>
+            show remaining time?
+          </button>
+        )}
+        {validationError.length > 0 && <p>{validationError}</p>}
+        {timeComplete && (
+          <div style={{fontSize: "6rem"}}>you're done yay!!!!!!!!!!!</div>
+        )}
+        {showRemaining && (
+          <ShowRemainingTime
+            timestampEnd={timeEnd}
+            stopCounting={stopCounting}
+            timeComplete={timeComplete}
+          />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/ShowRemainingTime.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from "react";
+
+const ShowRemainingTime = ({timestampEnd, stopCounting, timeComplete}) => {
+  const [remainingTime, updateRemainingTime] = useState("");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = Date.now();
+
+      const newDate = new Date(timestampEnd - now);
+
+      console.log(newDate);
+
+      const minutes = String(newDate.getMinutes());
+      const seconds = String(newDate.getSeconds());
+
+      //   updateRemainingTime(
+      //     `${newDate.getMinutes()}:${newDate.getSeconds()}`
+      //   );
+
+      updateRemainingTime(`${minutes}:${seconds.padStart(2, "0")}`);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [stopCounting]);
+
+  console.log(`stopCounting: ${stopCounting}`);
+
+  return (
+    <div>
+      <p>{remainingTime}</p>
+    </div>
+  );
+};
+
+export default ShowRemainingTime;
